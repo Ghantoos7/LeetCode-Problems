0dc65f871e054fc2c52bdfd1c4bcbe7ba49784a6
Georgio Ghnatios <94653514+Ghantoos7@users.noreply.github.com>
2024-01-29 17:56:38 +0200

    Time: 8 ms (99.46%) | Memory: 44.8 MB (88.27%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        int sat_count = 0;
+        int i = 0;
+        int g_len = g.length;
+        int s_len = s.length;
+
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int j = 0; // Index for cookies
+
+        while (i < g_len && j < s_len) {
+            if (s[j] >= g[i]) {
+                sat_count++;
+                i++;
+            }
+            j++;
+        }
+
+        return sat_count;
+    }
+}
