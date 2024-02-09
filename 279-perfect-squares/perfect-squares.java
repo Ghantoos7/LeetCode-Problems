5a262111bb83aa0f654f24bc466bb3844eae6e9e
Georgio Ghnatios <94653514+Ghantoos7@users.noreply.github.com>
2024-02-09 18:54:33 +0200

    Time: 0 ms (100.00%) | Memory: 40.4 MB (98.31%) - LeetSync

--- 279-perfect-squares/perfect-squares.java
+++ 279-perfect-squares/perfect-squares.java
@@ -1,19 +1,21 @@
 class Solution {
-    public int numSquares(int n) {
-        int[] dp = new int[n + 1];
-        
-        for (int i = 0; i <= n; i++) {
-            dp[i] = Integer.MAX_VALUE;
+    public static int numSquares(int n) {
+        while (n % 4 == 0) {  // 消掉4的因子
+            n /= 4;
         }
-        
-        dp[0] = 0;
-        
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j * j <= i; j++) {
-                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+
+        if (n % 8 == 7) {  // 只能4个数表示的
+            return 4;
+        }
+
+        for (int i = 0; i * i <= n; i++) {  // 去看看是2个数还是1个数
+            int a = (int) Math.sqrt(n - i * i);
+            if (a * a + i * i == n) {
+                return i > 0 && a > 0 ? 2 : 1;
             }
         }
-        
-        return dp[n];
+
+        return 3;  // 剩下的只能用3个数表示
     }
+
 }
