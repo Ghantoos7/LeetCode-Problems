8124375b077cc80a7441e86b910bb172f0cc511c
Georgio Ghnatios <94653514+Ghantoos7@users.noreply.github.com>
2024-03-15 13:46:13 +0200

    Time: 2 ms (73.94%) | Memory: 54.9 MB (13.98%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,49 +1,28 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        
-
         int size = nums.length;
-
-        if (size == 0 || size == 1){
+        if (size == 0 || size == 1) {
             return nums;
         }
-        int[] pre = prefixProduct(nums);
-        int[] suf = suffixProduct(nums);
 
-        int[] ans = new int[size];
+        int[] prefixProducts = new int[size];
+        int[] suffixProducts = new int[size];
+        int[] result = new int[size];
 
-        for (int i = 0; i < size; i++){
-            ans[i] = pre[i] * suf[i];
+        prefixProducts[0] = 1;
+        for (int i = 1; i < size; i++) {
+            prefixProducts[i] = prefixProducts[i - 1] * nums[i - 1];
         }
 
-        return ans;
-
-    }
-
-
-    public int[] prefixProduct(int[] nums){
-        int size = nums.length;
-
-        int ans[] = new int[size];
-        ans[size-1] = 1;
-
-        for (int i = size - 2 ; i >= 0; i--){
-            ans[i] = nums[i+1] * ans[i+1];
+        suffixProducts[size - 1] = 1;
+        for (int i = size - 2; i >= 0; i--) {
+            suffixProducts[i] = suffixProducts[i + 1] * nums[i + 1];
         }
 
-        return ans;
-    }
-
-       public int[] suffixProduct(int[] nums){
-        int size = nums.length;
-
-        int ans[] = new int[size];
-        ans[0] = 1;
-
-        for (int i = 1 ; i < size ; i++){
-            ans[i] = nums[i-1] * ans[i-1];
+        for (int i = 0; i < size; i++) {
+            result[i] = prefixProducts[i] * suffixProducts[i];
         }
 
-        return ans;
+        return result;
     }
 }
