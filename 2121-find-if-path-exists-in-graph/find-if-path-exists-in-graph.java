0f6db1ddc28ef53c16b4e9442aa85f0339b7b44d
Georgio Ghnatios <94653514+Ghantoos7@users.noreply.github.com>
2024-04-21 16:15:58 +0300

    Time: 106 ms (57.38%) | Memory: 142.7 MB (62.68%) - LeetSync

---
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Solution {
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        if (source == destination) return true; 
+
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        for (int[] edge : edges) {
+            graph.putIfAbsent(edge[0], new ArrayList<>());
+            graph.putIfAbsent(edge[1], new ArrayList<>());
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
+        }
+
+        Set<Integer> visited = new HashSet<>();
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(source);
+        visited.add(source);
+
+        // BFS loop
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+
+            for (int adj : graph.getOrDefault(node, Collections.emptyList())) {
+                if (adj == destination) return true; 
+                if (visited.add(adj)) { 
+                    queue.offer(adj);
+                }
+            }
+        }
+
+        return false;
+    }
+}
