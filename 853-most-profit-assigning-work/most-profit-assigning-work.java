717d5a9885c014efb8b44fc1d2d08dcd210a2cde
Georgio Ghnatios <94653514+Ghantoos7@users.noreply.github.com>
2024-06-18 18:15:51 +0300

    Time: 22 ms (49.14%) | Memory: 46 MB (18.30%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.java
+++ 853-most-profit-assigning-work/most-profit-assigning-work.java
@@ -1,28 +1,27 @@
 class Solution {
     public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
-        int max_profit = 0;
-
-        for (int i = 0; i < worker.length ; i++){
-
-            int max_profit_per_worker = 0 ;
-
-            for (int j = 0 ; j < profit.length ; j++){
-                
-
-                if (worker[i] >= difficulty[j]){
-
-                max_profit_per_worker = Math.max(max_profit_per_worker, profit[j]);
-                
-                }
-
-
-
+        int n = difficulty.length;
+        int[][] jobs = new int[n][2];
+        
+        for (int i = 0; i < n; i++) {
+            jobs[i][0] = difficulty[i];
+            jobs[i][1] = profit[i];
+        }
+        
+        Arrays.sort(jobs, (a, b) -> Integer.compare(a[0], b[0]));
+        Arrays.sort(worker);
+        
+        int maxProfit = 0, maxProfitPerWorker = 0, j = 0;
+        
+        for (int i = 0; i < worker.length; i++) {
+            while (j < n && worker[i] >= jobs[j][0]) {
+                maxProfitPerWorker = Math.max(maxProfitPerWorker, jobs[j][1]);
+                j++;
             }
-       
-
-            max_profit += max_profit_per_worker;
-
-        }    return max_profit;
+            maxProfit += maxProfitPerWorker;
+        }
+        
+        return maxProfit;
     }
 
 
