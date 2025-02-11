7ac96d04b90afe5e7e5aabc9e35d988b70133d26
Georgio Ghnatios <94653514+Ghantoos7@users.noreply.github.com>
2025-02-11 17:18:52 +0200

    Time: 1266 ms (16.03%) | Memory: 0B (100.00%) - LeetSync

--- 1523-capital-gainloss/capital-gainloss.sql
+++ 1523-capital-gainloss/capital-gainloss.sql
@@ -1,17 +1,5 @@
-# Write your MySQL query statement below
-
-with temp as (
-
-SELECT stock_name,
-    CASE 
-        WHEN operation = 'Buy' THEN price * -1
-        ELSE price
-    END AS price
-from Stocks 
-
-)
-
-
-select stock_name , SUM(price) as capital_gain_loss
-from temp
-group by stock_name
+Select stock_name, sum(case 
+    when operation = "Buy" then  - price
+    Else price end) as capital_gain_loss
+From Stocks
+Group by stock_name
