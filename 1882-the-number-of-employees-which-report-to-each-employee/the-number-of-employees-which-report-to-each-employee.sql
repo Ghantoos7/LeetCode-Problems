35063288f82de53b695238cdcf305a7cd3bafe23
Georgio Ghnatios <94653514+Ghantoos7@users.noreply.github.com>
2025-03-10 13:19:27 +0200

    Time: 1095 ms (62.10%) | Memory: 0B (100.00%) - LeetSync

--- 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
+++ 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
@@ -3,7 +3,7 @@
 
 SELECT E1.employee_id, E1.name, COUNT(E2.name) AS reports_count , ROUND(AVG(E2.age )) as average_age 
 FROM Employees E1
-LEFT JOIN Employees E2 ON E1.employee_id = E2.reports_to 
+INNER JOIN Employees E2 ON E1.employee_id = E2.reports_to 
 GROUP BY E1.employee_id
 HAVING reports_count > 0
 ORDER BY E1.employee_id
